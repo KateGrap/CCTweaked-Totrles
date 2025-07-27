a409b629ab289698a3b7d40e42c66cdba92e5ca9
Kate <000.grapenko@gmail.com>
2025-07-27 22:31:04 +0300

    Turtles with a check

---
+++ 0/move_in_for.lua
@@ -0,0 +1,13 @@
+while turtle.detect("Smooth Stone") do
+    for i = 1, 2 do
+        turtle.turnRight()
+    end
+end
+repeat 
+    turtle.forward()
+until turtle.detect("Smooth Stone")
+while turtle.detect("Smooth Stone") do
+    for i = 1, 2 do
+        turtle.turnRight()
+    end
+end
