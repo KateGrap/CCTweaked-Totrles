5982cb96794f4fd37a24bcb7da592a11e22abf48
Kate <000.grapenko@gmail.com>
2025-07-29 05:39:45 +0300

    Procedures

---
+++ 0/procedures.lua
@@ -0,0 +1,12 @@
+-- first procedures
+turtle.turnRight()
+turtle.turnRight()
+
+--secong procedures
+repeat 
+    turtle.turnLeft()
+    turtle.detect("Smooth Stone")
+    turtle.turnRight()
+    turtle.turnRight()
+until turtle.forward()
+
