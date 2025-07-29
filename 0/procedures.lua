845925a9170fadaafd5051592616aa662780a19d
Kate <000.grapenko@gmail.com>
2025-07-29 17:10:22 +0300

    second try procedures

--- 0/procedures.lua
+++ 0/procedures.lua
@@ -1,12 +1,15 @@
 -- first procedures
-turtle.turnRight()
-turtle.turnRight()
+function move(to)
+  turtle.turnLeft(to)
+  turtle.turnLeft(to)
+end
+
 
 --secong procedures
-repeat 
-    turtle.turnLeft()
-    turtle.detect("Smooth Stone")
-    turtle.turnRight()
-    turtle.turnRight()
-until turtle.forward()
-
+function move(to, search)
+     turtle.turnLeft(to)
+    turtle.detect(seatch "Smooth Stone")
+    turtle.turnRight(to)
+    turtle.turnRight(to)
+    rurtle.forward(to)
+end
