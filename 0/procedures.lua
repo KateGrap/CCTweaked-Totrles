72fe7d1b035cd42180ead3f294a998d9fa7de79a
Kate <000.grapenko@gmail.com>
2025-07-31 20:51:19 +0300

    ///

--- 0/procedures.lua
+++ 0/procedures.lua
@@ -8,3 +8,10 @@ end
 function rotate()
   for turtle.turnRight() 2
 end
+Check_Block_Forward = function(target)
+  target = target or "minecraft:cobblestone"
+  local success, data = turtle.inspect()
+  turtle.forward()
+  return success, data.name = target
+end
+  
